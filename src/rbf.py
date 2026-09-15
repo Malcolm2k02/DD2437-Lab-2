42eaf7ec21bc4bcf49dbedc9d912ee680e5b69f6
Malcolm Söyring Helasterä <malsh@kth.se>
2026-09-15 16:19:23 +0200

    setup

---
+++ src/rbf.py
@@ -0,0 +1,19 @@
+def gaussian_rbf(x, mu, sigma):
+    return ""
+
+
+def design_matrix(X, mu, sigma):
+    return ""
+
+
+def train_least_squares(X, y, centers, sigma):
+    return ""
+
+
+def predict_rbf(X, centers, sigma, weights):
+    return ""
+
+
+def train_delta(X, y, centers, sigma, eta, epochs):
+    return ""
+
